ed099f4d59aefcaf1068a1ddeda5947768d0484e
李佳辉 <lijiahui.hpl@bytedance.com>
2020-01-10 16:24:02 +0800

    is master
    Change-Id: Ib765e1d137822f24e29580833f6140e580b4bf36

--- src/test/java/yaofang/demo/DemoApplicationTests.java
+++ src/test/java/yaofang/demo/DemoApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
-
+   // it is master
     @Test
     public void contextLoads() {
     } // dsdsd
